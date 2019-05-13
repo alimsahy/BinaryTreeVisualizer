c212528c0a8c16335543a7bf8993f12bd3592895
Alimşah Yıldırım <pumpings@hotmail.com>
2019-05-14 02:25:14 +0300

    Update TreeNodeColor.cs

--- TreeNodeColor.cs
+++ TreeNodeColor.cs
@@ -1,4 +1,14 @@
-﻿namespace RedBlackTree
+/*
+ * 
+ * Create by Alimsah YILDIRIM on 3/5/2019
+ * 
+ * @file        TreeNodeColor.cs
+ * @url         github.com/alimsahy/BinaryTreeVisualizer
+ * 
+ * 
+*/
+
+namespace RedBlackTree
 {
     public enum TreeNodeColor
     {
